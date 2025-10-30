5a4096cf43618185733743d972dc0e367d9c0cc7
Sabbir-shekih <web.3.0.bd@gmail.com>
2025-10-30 09:29:00 -0700

    Update geminiService.ts

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -1,7 +1,7 @@
 
 import { GoogleGenAI } from "@google/genai";
 
-const API_KEY = process.env.API_KEY;
+const API_KEY = AIzaSyDD-ZYkq7gi_XsR_tDFAQTPqC4udSd4NAc;
 
 if (!API_KEY) {
   // In a real app, you might show an error to the user or disable AI features.
